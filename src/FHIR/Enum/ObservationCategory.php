8038623c5921ff9780490fc92634f41fc833446c
niikhsan <aikhsan17@gmail.com>
2024-02-10 08:03:14 +0700

    add column encounter&condition

--- src/FHIR/Enum/ObservationCategory.php
+++ src/FHIR/Enum/ObservationCategory.php
@@ -2,7 +2,36 @@
 
 namespace Niikhsan\SatusehatIntegrate\FHIR\Enum;
 
-enum ObservationCategory: string
+// enum ObservationCategory: string
+// {
+//     case VitalSigns = 'vital-signs';
+// }
+
+class ObservationCategory
 {
-    case VitalSigns = 'vital-signs';
+    private $__value;
+    private $__name;
+
+    const VitalSigns = 'vital-signs';
+
+    public function __construct(string $value)
+    {
+        $myClass = new ReflectionClass ( get_class($this) );
+        $constants = $myClass->getConstants();
+        foreach($constants as $categoryName=>$categoryValue) {
+            if($value === $categoryValue) {
+                $this->__category = $categoryName;
+                $this->__value = $categoryValue;
+                break;
+            }
+        }
+        if(is_null($this->__category)) {
+            throw new InvalidArgumentException("Invalid category of the week");
+        }
+    }
+
+    public function GetValue(): int
+    {
+        return $this->__value;
+    }
 }
